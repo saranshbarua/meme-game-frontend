d1dc03125ea4a0f0e0a4dfda2bda8cc37176176a
saranshbarua <saranshbarua@gmail.com>
2018-11-17 03:17:54 +0530

    add join room form ui

--- src/components/JoinRoom/joinRoom.js
+++ src/components/JoinRoom/joinRoom.js
@@ -3,8 +3,19 @@ import React, { Component } from 'react'
 export default class JoinRoom extends Component {
   render() {
     return (
-      <div>
-        Join Room
+      <div className="join-room">
+        <div className="paci logo">
+            Meme Lord
+        </div>
+        <form className="jr">
+          <input className="inpt ssp-400" type="text" placeholder="Enter username" />
+          <input className="inpt ssp-400" type="password" placeholder="Enter shared password" />
+          <button
+            className="enter-room ssp-400"
+          >
+            Enter
+          </button>
+        </form>
       </div>
     )
   }
