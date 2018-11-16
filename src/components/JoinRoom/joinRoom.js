f22b03a3c8bf16630111072c75d8ae5a8ee4cc28
saranshbarua <saranshbarua@gmail.com>
2018-11-17 04:11:47 +0530

    redirect user to play room

--- src/components/JoinRoom/joinRoom.js
+++ src/components/JoinRoom/joinRoom.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 export default class JoinRoom extends Component {
 
@@ -7,6 +8,7 @@ export default class JoinRoom extends Component {
     this.state = {
       username: '',
       password: '',
+      redirectToRoom: false
     }
   }
 
@@ -22,13 +24,30 @@ export default class JoinRoom extends Component {
     })
   }
 
+  joinRequest() {
+    if(this.state.password === 'pass'){
+      this.setState({
+        redirectToRoom: true
+      })
+    }
+  }
+
   render() {
+    if(this.state.redirectToRoom) {
+      return <Redirect to={{
+        pathname: `/play/${this.state.username}`,
+        state: {
+          userPlaying: this.state.username
+        }
+      }} />
+    }
+
     return (
       <div className="join-room">
         <div className="paci logo">
             Meme Lord
         </div>
-        <form className="jr">
+        <form onSubmit={(e) => {this.joinRequest(e)}} className="jr">
           <input value={this.state.username} onChange={(e) => (this.handleUsername(e))} className="inpt ssp-400" type="text" placeholder="Enter username" />
           <input value={this.state.password} onChange={(e) => (this.handlePassword(e))} className="inpt ssp-400" type="password" placeholder="Enter shared password" />
           <button
