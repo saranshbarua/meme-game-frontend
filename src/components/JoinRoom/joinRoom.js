d8356794b22bfda07d21ae970c8111b08217d913
saranshbarua <saranshbarua@gmail.com>
2018-11-17 03:48:01 +0530

    make controlled form

--- src/components/JoinRoom/joinRoom.js
+++ src/components/JoinRoom/joinRoom.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react'
 
 export default class JoinRoom extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
+  handleUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  handlePassword(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="join-room">
@@ -8,8 +29,8 @@ export default class JoinRoom extends Component {
             Meme Lord
         </div>
         <form className="jr">
-          <input className="inpt ssp-400" type="text" placeholder="Enter username" />
-          <input className="inpt ssp-400" type="password" placeholder="Enter shared password" />
+          <input value={this.state.username} onChange={(e) => (this.handleUsername(e))} className="inpt ssp-400" type="text" placeholder="Enter username" />
+          <input value={this.state.password} onChange={(e) => (this.handlePassword(e))} className="inpt ssp-400" type="password" placeholder="Enter shared password" />
           <button
             className="enter-room ssp-400"
           >
