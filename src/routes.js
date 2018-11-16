a17e96413e51a3191cd8f446700e2535d71d2c61
saranshbarua <saranshbarua@gmail.com>
2018-11-17 01:54:00 +0530

    customize react app and add routing

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom';
+
+//Components
+
+import Home from './components/Home/home';
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route path="/" exact component={Home} />
+        </Switch>
+    )
+}
+
+export default Routes;
